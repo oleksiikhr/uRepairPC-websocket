69c63a1c5e1a6ba5cc4cae20b979bf7ce0c28917
Alexey Khrushch <alexeykhr@outlook.com>
2019-04-20 23:42:03 +0300

    Some update logic to redis events

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const awaitRequest = require('./helper/await-request')
 const server = require('http').createServer()
 const io = require('socket.io')(server)
 const Redis = require('ioredis')
+const path = require('path')
 const env = require('./env')
 
 /*
@@ -15,18 +16,48 @@ const env = require('./env')
 const redis = new Redis
 const sockets = {}
 
-// redis.psubscribe('*')
-// redis.on('pmessage', (channel, pattern, message) => {
-//   console.log(channel, pattern, message)
-//   message = JSON.parse(message)
-//   io.emit(pattern + ':' + message.event, message.data)
-// })
+redis.psubscribe('server.*')
+redis.on('pmessage', (channel, pattern, message) => {
+  console.log(channel, pattern, message)
+
+	try {
+		message = JSON.parse(message)
+	} catch (e) {
+		return
+	}
+
+	switch (message.event) {
+	case `App${path.sep}Events${path.sep}Users${path.sep}Edit`:
+		sockets[message.data.from_id].socket
+			.to('users')
+			.emit(`users-${message.data.id}`, {
+				action: 'update',
+				detail: message.data.detail,
+				from_id: message.data.from_id
+			})
+		break
+	}
+})
 
 io.on('connection', (socket) => {
 	console.log('Connection', socket.id)
 
+	// FIXME fill on user Auth (permissions)
+	socket.join('users')
+
+	// TODO Auth
+	socket.on('auth', () => {
+		console.log('Auth')
+
+		// Check user from server
+		// FIXME 1 - example userId
+		sockets[1] = { socket }
+	})
+
 	socket.on('disconnect', () => {
 		console.log('Disconnect', socket.id)
+		// FIXME
+		delete sockets[1]
 	})
 })
 
