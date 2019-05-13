8fbba74d42c746605a8b1c0bd693a83a5db6b2b4
Alexey Khruhsch <alexeykhr@outlook.com>
2019-05-13 20:28:02 +0300

    chore: remove unused files

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,3 @@
-import { removeListSingleSlash } from './helper/utils';
-
-export const isProd: boolean = !['dev', 'development'].includes(process.env.NODE_ENV || 'production');
-
-export const laravelServer: string = removeListSingleSlash(process.env.LARAVEL_SERVER) || 'http://localhost';
-
 export const hostName: string|undefined = process.env.APP_HOSTNAME;
 
 export const port: number = typeof process.env.APP_PORT === 'undefined' ? 3000 : +process.env.APP_PORT;
