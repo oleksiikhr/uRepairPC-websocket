3d0edc068d5795c60ae269349b37ac8c4b7c9304
Alexey Khruhsch <alexeykhr@outlook.com>
2019-06-12 22:38:44 +0300

    chore: change structure for support autodeploy psubscribe

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@
 import http from 'http';
 import Redis from 'ioredis';
 import socketIO from 'socket.io';
-import allTypes, * as type from './enum/types';
 import * as env from './env';
+import autodeploySubscribeHandler from './psubscribe/autodeploy'
+import serverSubscribeHandler from './psubscribe/server'
 
 /* | -----------------------------------------------------------------------------------
  * | - Variables -
@@ -21,86 +22,17 @@ const io = socketIO(server);
  * | -----------------------------------------------------------------------------------
  */
 redis.psubscribe('server.*');
+redis.psubscribe('autodeploy.*');
 redis.on('pmessage', (channel, pattern, message) => {
-  /*
-   * Get data from laravel and validate to right structure
-   */
-  try {
-    message = JSON.parse(message).data;
-
-    if (!message.event) {
-      throw new Error('Event is empty');
-    }
-
-    if (typeof message.data === 'undefined') {
-      throw new Error('Data is undefined');
-    }
-
-    if (typeof message.rooms === 'undefined') {
-      throw new Error('Rooms is empty');
-    }
-
-    if (!allTypes.includes(message.type)) {
-      throw new Error('Type is invalid');
-    }
-
-    if (!message.socketId) {
-      throw new Error('SocketId is empty');
-    }
-  } catch (e) {
-    console.warn(e);
-    return;
-  }
-
-  const socket = io.sockets.connected[message.socketId];
-  if (typeof socket === 'undefined') {
-    return;
-  }
-
-  // Get rooms
-  let rooms: string[] = [];
-  if (Array.isArray(message.rooms)) {
-    rooms = message.rooms;
-  } else if (message.rooms) {
-    rooms = [message.rooms];
-  }
-
-  switch (message.type) {
-  case type.JOIN:
-    rooms.forEach((room) => socket.join(room));
-    break;
-  case type.SYNC:
-    /*
-     * Leave the existing rooms that are not found in the array
-     * and join the remaining rooms.
-     */
-    Object.keys(socket.rooms).forEach((room) => {
-      if (socket.id !== room) {
-        const findRoomIndex = rooms.indexOf(room);
-        if (~findRoomIndex) {
-          rooms.splice(findRoomIndex, 1);
-        } else {
-          socket.leave(room);
-        }
-      }
-    });
-    rooms.forEach((room) => socket.join(room));
-    break;
-  case type.CREATE:
-  case type.UPDATE:
-  case type.DELETE:
-    /*
-     * Broadcast event to rooms or all
-     */
-    rooms.forEach((room) => socket.to(room));
-    socket.broadcast.emit(message.event, {
-      data: message.data,
-      params: message.params,
-      type: message.type,
-    });
-    break;
-  default:
-    console.warn('Broadcast: message.type unknown');
+  switch (channel) {
+    case 'server.*':
+      serverSubscribeHandler(message, io.sockets);
+      break;
+    case 'autodeploy.*':
+      autodeploySubscribeHandler(message, io);
+      break;
+    default:
+      console.warn('[PMessage] ->', channel);
   }
 });
 
